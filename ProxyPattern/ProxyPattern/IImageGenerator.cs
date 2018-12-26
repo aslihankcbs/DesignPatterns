049f7ab4784027832d768badb0f66138eff34f32
aslihan <kocabasasli66@gmail.com>
2018-12-26 23:21:11 +0300

    ProxyPattern

---
+++ ProxyPattern/ProxyPattern/IImageGenerator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern
+{
+    //Subject
+    interface IImageGenerator
+    {
+        void ShowImage();
+    }
+
+    //real subject
+    class ImageGenerator : IImageGenerator
+    {
+        private String _fullPath;
+
+        public ImageGenerator(string fullPath)
+        {
+            _fullPath = fullPath;
+        }
+        public void ShowImage()
+        {
+            Console.WriteLine("{0} resmi gösteriliyor.", _fullPath);
+        }
+    }
+
+    //proxy
+
+    class ImageGeneratorProxy : IImageGenerator
+    {
+        private ImageGenerator _generator;
+        private string _fullPath;
+
+        public ImageGeneratorProxy(string fullPath)
+        {
+            _fullPath = fullPath;
+        }
+        public void ShowImage()
+        {
+            if (_generator == null)
+            {
+                _generator = new ImageGenerator(_fullPath);
+            }
+            _generator.ShowImage();
+        }
+    }
+
+
+
+
+
+
+
+
+
+}
+
+
---
+++ ProxyPattern/ProxyPattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ImageGeneratorProxy proxy1 = new ImageGeneratorProxy("c:\\resim1.jpg");
+            ImageGeneratorProxy proxy2 = new ImageGeneratorProxy("c:\\resim2.jpg");
+
+            proxy1.ShowImage();
+            proxy2.ShowImage();
+            proxy1.ShowImage();
+        }
+    }
+}
