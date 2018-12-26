45d740e1373750563d2a8dde9ec54fffa326dbd2
aslihan <kocabasasli66@gmail.com>
2018-12-26 23:20:13 +0300

    AdapterPattern

---
+++ AdapterPattern/AdapterPattern/IErrorModel.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    public interface IErrorModel
+    {
+        int ErrorNumber
+        {
+            get;
+            set;
+        }
+        string Description { get; set; }
+        void SendMail();
+    }
+
+    public class DbError : IErrorModel
+    {
+        private int _errorNumber;
+        private string _description;
+
+        public int ErrorNumber {
+            get
+            {
+                return _errorNumber;
+            }
+            set
+            {
+                _errorNumber = value;
+            }
+        }
+
+        public string Description {
+            get
+            {
+                return _description; 
+            }
+            set
+            {
+                _description = value;
+            }
+        }
+
+        public void SendMail()
+        {
+            Console.WriteLine("{0}{1} -> Db Hatası Gönderildi.",ErrorNumber.ToString(), Description);
+        }
+    }
+
+    //Servislerde oluşan hataları bünyesinde tutacak
+    public class ServiceError : IErrorModel
+    {
+        private int _errorNumber;
+        private string _description;
+
+        public int ErrorNumber
+        {
+          get
+          {
+             return _errorNumber;
+          }
+          set
+          {
+             _errorNumber = value;
+              
+          }
+        }
+
+        public string Description
+        {
+          get
+          {
+                return _description;
+          }
+          set
+          {
+                _description = value;
+          }   
+        }
+
+        public void SendMail()
+        {
+            Console.WriteLine("{0]{1} -> Service hatası gönderildi.",ErrorNumber.ToString(), Description);
+        }
+    }
+
+    //Adapter Class
+    public class FaxAdapter : IErrorModel
+    {
+        private Fax _fax; //adapte edilecek tip
+        public FaxAdapter(Fax fax)
+        {
+            _fax = fax; 
+        }
+
+        public int ErrorNumber
+        {
+            get
+            {
+                return _fax.FaxErrorCode;
+            }
+            set
+            {
+                _fax.FaxErrorCode = value;
+            }    
+        }
+        public string Description
+        {
+            get
+            {
+                return _fax.ErrorDescription;
+            }
+            set
+            {
+                _fax.ErrorDescription = value;
+            }
+        }
+
+        public void SendMail()
+        {
+            Console.WriteLine("{0}{1} -> Fax hatası alındı.", ErrorNumber.ToString(), Description);
+        }
+    }
+
+
+    //Adaptee yapısı -> Amacımız bu yapıyı kendi sistemimize adapte etmek
+    public class Fax
+    {
+        public int FaxErrorCode { get; set; }
+        public string ErrorDescription {get; set;}
+
+        void Send()
+        {
+
+        }
+
+        void Get()
+        {
+
+        }
+
+
+    }
+}
---
+++ AdapterPattern/AdapterPattern/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fax fax = new Fax{FaxErrorCode = 4000, ErrorDescription = "Karşı taraftan cevap gelmiyor."};
+
+            IErrorModel[] errors =
+            {
+                new DbError{ErrorNumber=1000, Description="Bağlantı sağlanamadı." },
+                new DbError{ErrorNumber=1001, Description="Sorgulama hatası." },
+                new ServiceError{ErrorNumber=3000, Description="Yetki yok." },
+                new ServiceError{ErrorNumber=3004,Description="Service bulunamadı." },
+                new FaxAdapter(fax)
+            };
+
+            foreach (IErrorModel error in errors)
+            {
+                error.SendMail();
+            }
+        }
+    }
+}
+
+
+
